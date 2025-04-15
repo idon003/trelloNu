b7b308b28a35802047fe541d148c4e827604a06a
A1dyn <65164525+idon003@users.noreply.github.com>
2025-04-15 19:17:07 +0500

    Update SecurityConfig.java

--- src/main/java/com/senior_project/configurations/SecurityConfig.java
+++ src/main/java/com/senior_project/configurations/SecurityConfig.java
@@ -20,9 +20,11 @@ public class SecurityConfig {
     public SecurityFilterChain securityFilterChain(HttpSecurity http, JwtAuthenticationFilter jwtAuthenticationFilter) throws Exception {
         http
                 .csrf().disable()
+                .cors() // включаем CORS
+                .and()
                 .authorizeRequests()
                 .requestMatchers("/api/auth/**", "/swagger-ui/**", "/swagger-ui.html", "/v3/api-docs/**").permitAll()
-                .anyRequest().authenticated() // All other endpoints require authentication.
+                .anyRequest().authenticated()
                 .and()
                 .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class)
                 .sessionManagement()
@@ -31,7 +33,20 @@ public class SecurityConfig {
     }
 
     @Bean
+    public CorsFilter corsFilter() {
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowCredentials(true);
+        config.setAllowedOrigins(Arrays.asList("http://localhost:5173")); // или "*" если нужно разрешить всем
+        config.setAllowedHeaders(Arrays.asList("Origin", "Content-Type", "Accept", "Authorization"));
+        config.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", config);
+        return new CorsFilter(source);
+    }
+
+    @Bean
     public PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();  // You can also use other encoders like Pbkdf2PasswordEncoder or Argon2PasswordEncoder
+        return new BCryptPasswordEncoder();
     }
 }
